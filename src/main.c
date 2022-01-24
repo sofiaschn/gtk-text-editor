e58bbf74c2bee0c1e48144e71db26f8b589a7b8b
Matheus Schneider Camilo <matheus.schneider.camilo@gmail.com>
2022-01-24 19:28:15 -0300

    First commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <gtk/gtk.h>
+
+static void onActive(GtkApplication *app) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Text Editor");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app =
+        gtk_application_new("com.gtk-text-editor", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(onActive), NULL);
+    
+    return g_application_run(G_APPLICATION(app), argc, argv);
+}
